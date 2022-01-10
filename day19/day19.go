3c6481144f2f3736beed8ccafe9ab92e6e99fd58
Kevin Weithers <kevinweithers@gmail.com>
2022-01-10 09:49:55 -0600

    ocean man

--- day19/day19.go
+++ day19/day19.go
@@ -26,6 +26,7 @@ type scanner struct {
 	beacons []beacon
 }
 
+//A rotation represents how to reorient a scanner
 type rotation struct {
 	x     int //These are either 0,1,2 representing x,y,z
 	x_dir int //These are either 1 or -1 representing positive or negative
@@ -35,6 +36,7 @@ type rotation struct {
 	z_dir int
 }
 
+//Absolute value of an int
 func abs(x int) int {
 	if x > 0 {
 		return x
@@ -43,6 +45,7 @@ func abs(x int) int {
 	}
 }
 
+//Manhattan distance
 func distance(a beacon, b beacon) int {
 	x := abs(a[0] - b[0])
 	y := abs(a[1] - b[1])
@@ -102,8 +105,11 @@ func main() {
 	properly_oriented := make(map[int]bool, 0)
 	properly_oriented[0] = true
 
-	beacon_locations := make([]beacon, 0)
-	beacon_locations = append(beacon_locations, beacon{0, 0, 0})
+	//Keep track of scanner locations so we can find the distances between them
+	scanner_locations := make([]beacon, 0)
+	//Scanner 0 is at 0,0,0. (beacon type is used as just a point in space here)
+	scanner_locations = append(scanner_locations, beacon{0, 0, 0})
+
 	//Until all the scanners are properly oriented
 	for len(properly_oriented) < len(scanners) {
 		for x := range matrices {
@@ -112,37 +118,40 @@ func main() {
 				if (!properly_oriented[x] && !properly_oriented[y]) || (properly_oriented[x] && properly_oriented[y]) {
 					continue
 				}
+
+				//Loop through all beacon pairs
 				beacon_pairs := make([]beaconPair, 0)
 				for i := range matrices[x] {
 					for j := range matrices[y] {
 						l := len(Intersection(matrices[x][i], matrices[y][j]))
+						//If two beacons on different scanners have 11 or more of the same distances to other beacons, they are the same beacon!
 						if l >= 11 {
 							beacon_pairs = append(beacon_pairs, beaconPair{scanners[x].beacons[i], scanners[y].beacons[j]})
 						}
 					}
 				}
+				//If we find 12 beacon pairs, we know we can reorient scanner with respect to the other
 				if len(beacon_pairs) >= 12 {
-					// fmt.Println(x, y)
 					if properly_oriented[x] {
 						r, x_diff, y_diff, z_diff := find_rotation(beacon_pairs, rotations)
 						scanners[y] = reorient(&scanners, r, y, x_diff, y_diff, z_diff)
 						properly_oriented[y] = true
-						beacon_locations = append(beacon_locations, beacon{-x_diff, -y_diff, -z_diff})
+						scanner_locations = append(scanner_locations, beacon{-x_diff, -y_diff, -z_diff})
 					} else if properly_oriented[y] {
 						beacon_pairs := flip_beacon_pairs(beacon_pairs)
 						r, x_diff, y_diff, z_diff := find_rotation(beacon_pairs, rotations)
 						scanners[x] = reorient(&scanners, r, x, x_diff, y_diff, z_diff)
 						properly_oriented[x] = true
-						beacon_locations = append(beacon_locations, beacon{-x_diff, -y_diff, -z_diff})
+						scanner_locations = append(scanner_locations, beacon{-x_diff, -y_diff, -z_diff})
 					} else {
-						panic("This should never happen.")
+						panic("Neither beacon is properly oriented. This should never happen.")
 					}
 				}
 			}
 		}
 	}
 
-	//After we have reoriented all of our beacons, count how many unique beacons there are
+	//After we have reoriented all of our scanners, count how many unique beacons there are
 	beacon_map := make(map[beacon]bool)
 	for _, s := range scanners {
 		for _, b := range s.beacons {
@@ -150,9 +159,10 @@ func main() {
 		}
 	}
 	fmt.Println("Part 1:", len(beacon_map))
-	fmt.Println("Beacon locations", find_max_distance(beacon_locations))
+	fmt.Println("Part 2:", find_max_distance(scanner_locations))
 }
 
+//Finds the intersection of two slices
 func Intersection(a, b []int) (c []int) {
 	m := make(map[int]bool)
 
@@ -168,10 +178,10 @@ func Intersection(a, b []int) (c []int) {
 			c = append(c, item)
 		}
 	}
-	return
+	return c
 }
 
-//Take two beacons and find the proper orientatoin of the second one relative to the first one
+//Take two beacons and find the proper orientation of the second one relative to the first one
 func find_rotation(beacon_pairs []beaconPair, rotations []rotation) (rotation, int, int, int) {
 	for _, r := range rotations {
 		x_vals := make([]int, 0)
@@ -184,15 +194,13 @@ func find_rotation(beacon_pairs []beaconPair, rotations []rotation) (rotation, i
 		}
 		//If all three of these are all the same, it is the location of the rotated scanner relative to the original scanner
 		if allSameValue(x_vals) && allSameValue(y_vals) && allSameValue(z_vals) {
-			fmt.Println("SOLVED", r, x_vals[0], y_vals[0], z_vals[0])
-			return r, x_vals[0], y_vals[0], z_vals[0] //These are the diffs we can use for the equation
-			//Diff = dir*rotated - original
-			//Original = dir*rotated - diff
+			return r, x_vals[0], y_vals[0], z_vals[0] //These are the diffs we can use for the reorient equation
 		}
 	}
 	return rotation{}, -1, -1, -1
 }
 
+//Determine if all slice values are the same
 func allSameValue(a []int) bool {
 	for i := 1; i < len(a); i++ {
 		if a[i] != a[0] {
@@ -202,9 +210,8 @@ func allSameValue(a []int) bool {
 	return true
 }
 
-//x is the scanner number in the proper orientation, y is the scanner number in a rotated orientation
-//This re-orients a scanner's beacons by applying the correct rotation found using find_rotations
-//Uses the formula: Original_orientation = dir*rotated_orientation - diff to convert a rotated point to a properly oriented point
+//This re-orients scanner y's beacons by applying the correct rotation found using find_rotations
+//Uses the formula: Original_orientation = dir*rotated_orientation - diff
 func reorient(scanners *[]scanner, r rotation, y int, x_diff, y_diff, z_diff int) scanner {
 	reoriented_beacons := make([]beacon, 0)
 	for _, beacon := range (*scanners)[y].beacons {
@@ -226,6 +233,7 @@ func flip_beacon_pairs(bp []beaconPair) []beaconPair {
 	return new_pairs
 }
 
+//Find the max distance between any two beacons
 func find_max_distance(b []beacon) int {
 	max_dist := 0
 	for i := range b {

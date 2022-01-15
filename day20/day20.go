76ddf0e7861e5fdd5fa78a0b96f2b02db122d12b
Kevin Weithers <kevinweithers@gmail.com>
2022-01-15 14:33:04 -0600

    buzz lightyear

--- day20/day20.go
+++ day20/day20.go
@@ -8,17 +8,14 @@ import (
 )
 
 const n = 100
-
 const algo = "#.#.....##...##..#.#......#.#...#.#.#...###.##......###.##.##..##.#...#.....###.#.....#.#.#...#.#..###.###..###..#..##..###..##..##.##.#..###########.##....#.#......#...#.###..###...#.####..########.#####.#.#..##.##.##..###.##.####.#..##.##..#...#####..#.#.##.##...##..#..##.....###.#.#....####.##.#...##.########.#.##.#.....###....#..###.####....############.#.##...#.####...#...##.#.#..#..#......#..##...#.########.#.#...#####..#..######.#.#.....#####...##.###.#.#.##.........#.#.##..##.#..#..##..##.###.##.##."
-
-// const algo = "..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#"
 
 type point struct {
 	y int
 	x int
 }
 
-type Image struct {
+type image struct {
 	points         map[point]bool
 	x_min          int
 	x_max          int
@@ -27,7 +24,7 @@ type Image struct {
 	infinity_point bool
 }
 
-func (i Image) get(y, x int) bool {
+func (i image) get(y, x int) bool {
 	if y < i.y_min || y > i.y_max || x < i.x_min || x > i.x_max {
 		return i.infinity_point
 	} else {
@@ -50,17 +47,15 @@ func main() {
 		j++
 	}
 
-	i := Image{m, 0, n - 1, 0, n - 1, false}
-	i = enhance(i)
-	i = enhance(i)
-
-	c := 0
-	for _, v := range i.points {
-		if v {
-			c++
+	i := image{m, 0, n - 1, 0, n - 1, false}
+	for t := 0; t < 50; t++ {
+		i = enhance(i)
+		if t == 1 {
+			fmt.Println("Part 1:", count_lights(i))
 		}
 	}
-	fmt.Println(c)
+	fmt.Println("Part 2:", count_lights(i))
+
 }
 
 func BinToDec(bin string) int {
@@ -68,7 +63,7 @@ func BinToDec(bin string) int {
 	return int(x)
 }
 
-func enhance(im Image) Image {
+func enhance(im image) image {
 	new_map := make(map[point]bool, 0)
 	for j := im.y_min - 1; j <= im.y_max+1; j++ {
 		for i := im.x_min - 1; i <= im.x_max+1; i++ {
@@ -76,7 +71,7 @@ func enhance(im Image) Image {
 		}
 	}
 
-	return Image{
+	return image{
 		points:         new_map,
 		x_min:          im.x_min - 1,
 		x_max:          im.x_max + 1,
@@ -85,7 +80,7 @@ func enhance(im Image) Image {
 		infinity_point: !im.infinity_point}
 }
 
-func process_point(im Image, y, x int) bool {
+func process_point(im image, y, x int) bool {
 	s := ""
 	for j := -1; j <= 1; j++ {
 		for i := -1; i <= 1; i++ {
@@ -101,4 +96,14 @@ func process_point(im Image, y, x int) bool {
 	} else {
 		return false
 	}
+}
+
+func count_lights(im image) int {
+	c := 0
+	for _, v := range im.points {
+		if v {
+			c++
+		}
+	}
+	return c
 }

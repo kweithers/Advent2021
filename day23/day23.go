f7bb1e4ea5e06482a9fe4bf835eb0d5e90512a7f
Kevin Weithers <kevinweithers@gmail.com>
2022-01-30 17:53:29 -0600

    room_to_room working

--- day23/day23.go
+++ day23/day23.go
@@ -26,7 +26,7 @@ type State struct {
 
 var starting_state = State{hallway: Hallway{46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46}, rooms: [4]Room{{68, 68}, {65, 67}, {67, 66}, {65, 66}}, cost: 0}
 var goal_state = State{hallway: Hallway{46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46}, rooms: [4]Room{{65, 65}, {66, 66}, {67, 67}, {68, 68}}, cost: math.MaxInt}
-var test_state = State{hallway: Hallway{66, 46, 46, 46, 46, 66, 46, 46, 46, 46, 46}, rooms: [4]Room{{65, 65}, {46, 46}, {67, 67}, {68, 68}}, cost: 0}
+var test_state = State{hallway: Hallway{46, 66, 46, 46, 46, 46, 46, 46, 46, 46, 67}, rooms: [4]Room{{65, 65}, {46, 66}, {66, 67}, {68, 68}}, cost: 0}
 
 //How much energy it costs each pod to move one square
 var costs = map[rune]int{65: 1, 66: 10, 67: 100, 68: 1000}
@@ -38,7 +38,8 @@ var entrances = map[int]int{0: 2, 1: 4, 2: 6, 3: 8}
 var homes = map[rune]int{65: 0, 66: 1, 67: 2, 68: 3}
 
 func main() {
-	x := hallway_to_room(test_state)
+	x := generate_moves(starting_state)
+	displayState(test_state)
 	fmt.Println("START")
 	for _, st := range x {
 		displayState(st)
@@ -129,7 +130,6 @@ Hallway_Walk:
 		//Check if all spaces along the way are open
 		start := hallway_index
 		finish := entrances[homes[pod]]
-		fmt.Println(start, finish)
 		if start > finish {
 			start, finish = finish, start
 		}
@@ -158,8 +158,71 @@ Hallway_Walk:
 	return new_states
 }
 
-// func room_to_room(st State) []State {}
-// func generate_moves(State) []State {}
+func room_to_room(st State) []State {
+	//Collect all new states that can be created by moving ANY pod in this state
+	var new_states []State
+
+	for room_index, room := range st.rooms {
+	Room_Walk:
+		for room_depth, pod := range room {
+			//If this is the bottom space in a room and we are blocked, continue
+			if room_depth == 1 && room[0] != 46 {
+				continue
+			}
+			//If this spot is empty, continue
+			if pod == 46 {
+				continue
+			}
+			//If this pod is already home, continue
+			if homes[pod] == room_index {
+				continue
+			}
+			//Check if this pod's home can be moved into
+			if !(st.rooms[homes[pod]] == Room{46, 46} || st.rooms[homes[pod]] == Room{46, pod}) {
+				continue
+			}
+
+			//Check if all spaces along the way are open
+			start := entrances[room_index]
+			finish := entrances[homes[pod]]
+			if start > finish {
+				start, finish = finish, start
+			}
+			for i := start; i <= finish; i++ {
+				if st.hallway[i] != 46 { //If this pod can't get home
+					continue Room_Walk
+				}
+			}
+
+			//Now, we know this pod can get home
+			new_state := st
+			//Remove this pod from its current spot
+			new_state.rooms[room_index][room_depth] = 46
+			//Place this pod at the correct depth in its room
+			correct_depth := 1
+			if st.rooms[homes[pod]][1] == pod {
+				correct_depth = 0
+			}
+			new_state.rooms[homes[pod]][correct_depth] = pod
+			//Increase the cost
+			move_cost := costs[pod] * (absDiffInt(start, finish) + (correct_depth + 1) + (room_depth + 1))
+			new_state.cost += move_cost
+			//Record this new possible state
+			new_states = append(new_states, new_state)
+		}
+	}
+	return new_states
+}
+
+func generate_moves(st State) []State {
+	var new_states []State
+
+	new_states = append(new_states, room_to_hallway(st)...)
+	new_states = append(new_states, hallway_to_room(st)...)
+	new_states = append(new_states, room_to_room(st)...)
+
+	return new_states
+}
 
 func absDiffInt(x, y int) int {
 	if x < y {
@@ -169,7 +232,6 @@ func absDiffInt(x, y int) int {
 }
 
 func displayState(st State) {
-	// don't need index so _
 	outString := ""
 	for _, v := range st.hallway {
 		outString += string(v)
